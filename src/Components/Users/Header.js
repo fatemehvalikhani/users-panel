2b1f8f6c2bf3ff0b8862d112bfe2b2785a49960a
fatemehvalikhani2015@gmail.com <fatemehvalikhani2015@gmail.com>
2022-05-10 04:03:32 +0430

    added header

---
+++ src/Components/Users/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header(){
+    return(
+           <div className="nav text-center bg-light">
+                <ul className="nav nav-pills  navbar-dark bg-dark shadow-smnavbar-dark bg-dark shadow-sm card-header-pills">
+                    <li className=""ss="nav-item">
+                        <a className="nav-link active" href="#">Users Panel</a>
+                    </li>
+                </ul>
+           </div>
+    )
+
+}
+export default Header;
